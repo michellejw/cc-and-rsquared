4cb20b551b9304efecac69afd88ac34832bcee74
michellejw <michellejw@gmail.com>
2019-02-14 13:37:07 -0500

    added dax notebook, added 2nd ice cream data script

---
+++ ice-cream-data2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Create an example dataset
+
+Created on Thu Feb 14 13:00:11 2019
+
+@author: michw
+"""
+
+import pandas as pd
+
+data_url = "https://michw.com/DATA/precip_temp_data.xlsx"
