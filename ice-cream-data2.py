40b00ef1f5b1c6701acb55c39766a19f685f8fb2
michellejw <michellejw@gmail.com>
2019-02-14 21:42:41 -0500

    added new files for ice cream data - attempt 2

--- ice-cream-data2.py
+++ ice-cream-data2.py
@@ -11,3 +11,13 @@ Created on Thu Feb 14 13:00:11 2019
 import pandas as pd
 
 data_url = "https://michw.com/DATA/precip_temp_data.xlsx"
+
+# Read ice cream data into a data frame
+df = pd.read_excel(data_url)
+
+# Make column names easier to work with
+df.columns = ['Year','Month','dayOfMonth','JulDay','NormalMaxTemp',\
+              'NormalMinTemp','MeanTemp','NormalPrecip']
+
+# Create a new temperature vector that is 
+
